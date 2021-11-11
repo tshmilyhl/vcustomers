24a9e8821d5d175da07e8ea3898bd8bdf1c6b3d0
tshmilyhl <985154610@qq.com>
2021-11-11 21:56:01 +0800

    parts of pages have completed

---
+++ src/components/Add.vue
@@ -0,0 +1,87 @@
+<template>
+<div class="add container">
+  <h1 class="page-header">添加用户</h1>
+  <form v-on:submit="addCustomer">
+    <div class="well">
+      <h4>用户信息</h4>
+      <div class="form-group">
+        <label>姓名</label>
+        <input type="text" class="form-control" placeholder="name" v-model="customer.name">
+      </div>
+
+      <div class="form-group">
+        <label>电话</label>
+        <input type="text" class="form-control" placeholder="phone" v-model="customer.phone">
+      </div>
+      <div class="form-group">
+        <label>邮箱</label>
+        <input type="text" class="form-control" placeholder="email" v-model="customer.email">
+      </div>
+
+      <div class="form-group">
+        <label>学历</label>
+        <input type="text" class="form-control" placeholder="education" v-model="customer.education">
+      </div>
+
+      <div class="form-group">
+        <label>毕业学校</label>
+        <input type="text" class="form-control" placeholder="graduationscholl" v-model="customer.graduationscholl">
+      </div>
+
+      <div class="form-group">
+        <label>职业</label>
+        <input type="text" class="form-control" placeholder="profession" v-model="customer.profession">
+      </div>
+
+      <div class="form-group">
+        <label>profile</label>
+<!--        <input type="text" class="form-control" placeholder="profile" v-model="customer.profile">-->
+        <textarea class="form-control" rows="10" v-model="customer.profile"></textarea>
+      </div>
+      <button type="submit" class="btn btn-primary">添加</button>
+    </div>
+
+  </form>
+</div>
+</template>
+
+<script>
+export default {
+  name: "Add",
+  data(){
+    return {
+      customer:{
+
+      }
+    }
+  },
+  methods:{
+    addCustomer(e){
+      if(!this.customer.name || ! this.customer.phone || ! this.customer.email){
+        console.log("请添加对应的信息")
+      } else {
+        let newCustomer = {
+          name:this.customer.name,
+          phone:this.customer.phone,
+          email:this.customer.email,
+          education:this.customer.education,
+          graduationschool:this.customer.graduationschool,
+          profession:this.customer.profession,
+          profile:this.customer.profile,
+        }
+        this.$http.post("http://localhost:3000/users",newCustomer)
+        .then(res=>{
+          this.$router.push({
+            path:'/'
+          })
+        })
+      }
+       e.preventDefault()
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Customers.vue
@@ -0,0 +1,48 @@
+<template>
+ <div class="customers container">
+   <h1 class="page-header">用户管理系统</h1>
+   <table class="table table-striped">
+     <thead>
+     <tr>
+       <th>姓名</th>
+       <th>电话</th>
+       <th>邮箱</th>
+       <th></th>
+     </tr>
+     </thead>
+     <tbody>
+     <tr v-for="customer in customers">
+       <td >{{customer.name}}</td>
+       <td>{{customer.phone}}</td>
+       <td>{{customer.email}}</td>
+       <td></td>
+     </tr>
+     </tbody>
+   </table>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'Customers',
+  data () {
+    return {
+  customers:[]
+    }
+  },
+  created(){
+    this.fetchCustomers()
+  },
+  methods:{
+    fetchCustomers(){
+      this.$http.get("http://localhost:3000/users")
+      .then(res=>{
+        console.log(res)
+        this.customers = res.data
+      })
+    }
+  }
+}
+</script>
+
+</style>
